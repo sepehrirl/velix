0fb2443c965806e2a0f69f84c7f447dcf0f8fb85
SEPEHR <sepehr2sodoury@gmail.com>
2026-09-17 04:00:00 +0330

    fix: add explicit URL test import extension

--- tests/core/url.test.ts
+++ tests/core/url.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { parseVideoUrl } from "../../src/core/url";
+import { parseVideoUrl } from "../../src/core/url.js";
 
 describe("parseVideoUrl", () => {
   it("accepts a public http URL", () => {
